d502b4e302a2a825369a5c5a2aaf9b06fd9ebc52
Amirbek2506 <62469848+Amirbek2506@users.noreply.github.com>
2020-05-01 10:38:23 +0500

    sozdat class Matrix and GetChar()

--- Program.cs
+++ Program.cs
@@ -1,11 +1,37 @@
 ﻿using System;
 
+
 namespace MyprojecsApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
+    }
+    class Matrix
+    {
+        Random rand;
+
+        const string litters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        public int Colunm { get; set; }
+
+        public bool NeedSecond { get; set; }
+
+        public Matrix(int col, bool needSecond)
+        {
+            Colunm = col;
+            rand = new Random((int)DateTime.Now.Ticks);
+            NeedSecond = needSecond;
+        }
+
+        char GetChar()
+        {
+            return litters.ToCharArray()[rand.Next(0, 35)];
+        }
+
+
     }
 }
